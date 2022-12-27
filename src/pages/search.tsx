5bcc2e999be5788f622336548112263e47f780c1
yuna Kim <yuna000804@gmail.com>
2022-12-27 23:45:44 +0900

    feat: 장소 정보 카드 생성

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -5,54 +5,84 @@ import axios from 'axios';
 import { useRef, useEffect } from "react";
 
 interface placeInforms {
-  place_id: string,
   name: string,
-  photos: string[],
+  reviews: string[],
+  photos: photosType[],
   formatted_address: string,
+  formatted_phone_number: string,
+  website: string,
+  rating: number,
 }
 
+interface photosType {
+  photo_reference: string,
+}
 
 function Search() {
   let [inform, setInform] = useState<placeInforms>();
   // let [inform, setInform] = useState<placeInforms>();
   let [place, setPlace] = useState('');
+  let [placeId, setPlaceId] = useState('');
   let [search, setSearch] = useState(false);
-  // console.log(search);
-  // if(search === true) {
+  let [count, setCount] = useState(0);
+
+  // const fetchData = () => {
+  //   console.log('clicked');
+  //   // setPlace(''); setInform(undefined);
   //   axios.get('/maps'+`/api/place/textsearch/json?query=${place}&key=AIzaSyA7uIJhOTUODaL2FW7MBDqQzoG043xKnSk`)
-  //   .then((res) => {console.log(res.data.results); setInform(res.data.results)})
-  //   .catch(() => {
-  //     console.log('실패');
-  //   })
+  //     .then((res) => {setPlaceId(res.data.results[0].place_id); console.log(placeId)})
+  //     .catch(() => {
+  //       console.log('실패');
+  //     });
+      
+  //     axios.get('/maps'+`/api/place/details/json?fields=name%2Crating%2Cformatted_phone_number%2Cformatted_address%2Cphoto%2Cwebsite%2Creviews&place_id=${placeId}&key=AIzaSyA7uIJhOTUODaL2FW7MBDqQzoG043xKnSk`)
+  //     .then((res) => {console.log(res.data.result.photos); setInform(res.data.result)})
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
   // }
-  
 
+    
   useEffect(() => {
-    setSearch(false);
-  }, [])
-  
-
-  useEffect(() => {
-    // const fetchData = async() => {
+      console.log('렌더');
+      console.log(place);
+      console.log(placeId);
+      
+      //placeId 가 업뎃이 바로 안됨 placeId 가 바뀌기 전에 다음 request 가 진행되어 업뎃이 안되는거였음
       axios.get('/maps'+`/api/place/textsearch/json?query=${place}&key=AIzaSyA7uIJhOTUODaL2FW7MBDqQzoG043xKnSk`)
-      .then((res) => {setInform(res.data.results[0])})
+      .then((res) => {setPlaceId(res.data.results[0].place_id);})
       .catch(() => {
         console.log('실패');
       });
-    // }
-    // fetchData();
-    console.log(inform);
-    // console.log(inform.name);
-  }, [search]);
+      
+      
+      // axios.get('/maps'+`/api/place/details/json?fields=name%2Crating%2Cformatted_phone_number%2Cformatted_address%2Cphoto%2Cwebsite%2Creviews&place_id=${placeId}&key=AIzaSyA7uIJhOTUODaL2FW7MBDqQzoG043xKnSk`)
+      // .then((res) => {console.log(res.data.result.photos); setInform(res.data.result)})
+      // .catch((err) => {
+      //   console.log(err);
+      // });
+      
+    
+      // axios.get('/maps'+`/api/place/photo?maxwidth=400&photo_reference=${inform?.photos[0].photo_reference}&key=AIzaSyA7uIJhOTUODaL2FW7MBDqQzoG043xKnSk`)
+      // .then((res) => {console.log(res)})
+      // console.log(inform);
+  }, [count]);
+
+  useEffect(() => {
+    axios.get('/maps'+`/api/place/details/json?fields=name%2Crating%2Cformatted_phone_number%2Cformatted_address%2Cphoto%2Cwebsite%2Creviews&place_id=${placeId}&key=AIzaSyA7uIJhOTUODaL2FW7MBDqQzoG043xKnSk`)
+    .then((res) => {console.log(res.data.result.photos); setInform(res.data.result)})
+    .catch((err) => {
+      console.log(err);
+    });
+  }, [placeId]);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.searchBar}>
-      {/* <input onChange={(e) => {setPlace(e.target.value)}}></input>
-      <button onClick={() => {setSearch(true)}}>검색</button> */}
         <InputGroup className={styles.input}>
         <Form.Control
-          onClick={() => {setSearch(false)}}
-          onChange={(e) => {setPlace(e.target.value)}}
+          // onClick={() => {setSearch(false)}}
+          onChange={(e) => {setPlace(e.target.value); console.log(place, e.target.value)}}
           placeholder="공간을 검색해주세요 !"
           aria-label="Recipient's username"
           aria-describedby="basic-addon2"
@@ -60,7 +90,11 @@ function Search() {
         <Button
           variant="outline-secondary" 
           className={styles.button}
-          onClick={() => {setSearch(true)}}
+          onClick={() => {
+            setSearch(true);
+            setCount((prev) => (prev+1))
+            // setCount((prev)=>(prev+1)); console.log(place) 
+          }}
         >
           <img src='/search.png' className={styles.searchImg} />
         </Button>
@@ -76,22 +110,29 @@ function Search() {
           : 
           <div className={styles.cardBox}>
               <span className={styles.title}>{inform?.name}</span>
+              {/* <div className={styles.item}> */}
               <div className={styles.contentBox}>
                 <span className={styles.address}>
                   <img src='/spot.png' />
                   {inform?.formatted_address}
                 </span>
+              {/* </div> */}
+              {/* <div className={styles.item}> */}
                 <span className={styles.website}>
                   <img src='/website.png' />
-                  대충 웹사이트
+                  {inform?.website}
                 </span>
+                {/* </div>
+                <div className={styles.item}> */}
                 <span className={styles.contact}>
                   <img src='/contact.png' />
-                  대충 전번
+                  {inform?.formatted_phone_number}
                 </span>
+                {/* </div> */}
               </div>
               <div className={styles.imgWrapper}>
-                <img className={styles.placeImg} src='/dog.png' />
+                <img className={styles.placeImg} src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&photo_reference=${inform?.photos[0].photo_reference}&key=AIzaSyA7uIJhOTUODaL2FW7MBDqQzoG043xKnSk`}/>
+                {/* <img className={styles.placeImg} src={'/dog.png'}/> */}
               </div>
             </div>
         } 
