cdddec2d42766587e5751e2e70d33a05f9804728
yuna Kim <yuna000804@gmail.com>
2022-12-31 05:21:53 +0900

    fix : netlify.toml 수정

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -39,7 +39,7 @@ function Search() {
     }
     else {
       // getPlace();
-      axios.get(`${PROXY}/maps/api/place/textsearch/json?query=${place}&key=${API_KEY}`)
+      axios.get(`${PROXY}/maps/api/place/textsearch/json?query=${place}&key=AIzaSyA7uIJhOTUODaL2FW7MBDqQzoG043xKnSk`)
       // axios.get(`https://maps.googleapis.com/maps/api/place/details/json?fields=/json?query=${place}&key=${API_KEY}`)
       .then((res) => { console.log(place, placeId); setPlaceId(res.data.results[0].place_id); setNext((prev)=>(prev+1))})
       .catch((err) => {
@@ -57,7 +57,7 @@ function Search() {
     }
     else {
       setNoData(false);
-      axios.get(`${PROXY}/maps/api/place/details/json?fields=name%2Ctypes%2Crating%2Cformatted_phone_number%2Cformatted_address%2Cphoto%2Cwebsite%2Creviews%2Cwheelchair_accessible_entrance%2Cgeometry&place_id=${placeId}&key=${API_KEY}`)
+      axios.get(`${PROXY}/maps/api/place/details/json?fields=name%2Ctypes%2Crating%2Cformatted_phone_number%2Cformatted_address%2Cphoto%2Cwebsite%2Creviews%2Cwheelchair_accessible_entrance%2Cgeometry&place_id=${placeId}&key=AIzaSyA7uIJhOTUODaL2FW7MBDqQzoG043xKnSk`)
       // axios.get(`https://maps.googleapis.com/maps/api/place/details/json?fields=name%2Ctypes%2Crating%2Cformatted_phone_number%2Cformatted_address%2Cphoto%2Cwebsite%2Creviews%2Cwheelchair_accessible_entrance%2Cgeometry&place_id=${placeId}&key=${API_KEY}`)
       .then((res) => {console.log(res); setSearch(true); setInform(res.data.result)})
       .catch((err) => {
